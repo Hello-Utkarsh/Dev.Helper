ea88122486d4cc6a2329da83dcb2cbc01dec3bd6
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-11-12 11:25:17 +0530

    adde the form

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,9 +1,12 @@
+import { Sql } from '@prisma/client/runtime/library'
 import React from 'react'
 
 const NewIssue = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center h-60 justify-around mt-20'>
+      <textarea rows={1} placeholder='Title' className='resize-none h-10 w-2/5 pt-2 px-3 rounded-lg'/>
+      <textarea rows={1} placeholder='Description' className='resize-none h-24 w-2/5 pt-2 px-3 rounded-lg'/>
+      <button className='bg-[#457b9d] text-[#f1faee] w-20 rounded-md h-7 font-medium'>Create</button>
     </div>
   )
 }
--- app/issue/page.tsx
+++ app/issue/page.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const IssuePage = () => {
   return (
-    <div className='flex justify-center'>
-      <div className='flex w-2/4 justify-around items-center mt-10'>
-        <input type="text" placeholder='Search...' name="" id="" className='w-4/5 h-9 p-4 rounded-lg' />
-        <button className='my-auto bg-[#457b9d] text-[#f1faee] w-20 rounded-md h-7 font-medium'>Search</button>
+    <div className='flex justify-end'>
+      <div className='flex w-4/5 justify-around items-center mt-10'>
+        <div className='flex w-2/4 justify-around items-center'>
+          <input type="text" placeholder='Search...' name="" id="" className='w-10/12 h-9 p-4 rounded-lg' />
+          <button className='my-auto bg-[#457b9d] text-[#f1faee] w-20 rounded-md h-7 font-medium'>Search</button>
+        </div>
+        <Link href='/issue/new'><button className='my-auto bg-[#457b9d] text-[#f1faee] w-28 rounded-md h-7 font-medium'>Create Issue</button></Link>
       </div>
     </div>
   )
